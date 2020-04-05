fcbc43381f33ffc8fb6ed76c343496061ec4fe64
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-05 11:24:05 -0400

    feat(core): Assert if the sources count are equal or greater than template sources model

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,26 @@
 import { loadJimp } from './lib';
 import { ArmariosPaginaCoreOptions } from './types';
+import { generateValidator } from './lib/node-assert';
+
+const assertions = {
+  optionsValidate: ({ sources, template }: ArmariosPaginaCoreOptions) => {
+    if (sources.length < template.sources.length) {
+      return {
+        message: `[error] This template requires ${template.sources.length} sources but it's only given ${sources.length}.`,
+        code: 1,
+      };
+    }
+
+    return {
+      code: 0,
+    };
+  },
+};
 
 export default async (options: ArmariosPaginaCoreOptions) => {
+  const assert = generateValidator(options);
+  assert(assertions.optionsValidate, options);
+
   const { template, sources } = options;
   const base = await loadJimp(template.base);
 
