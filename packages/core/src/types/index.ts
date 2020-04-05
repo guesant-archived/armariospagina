85e6dca9b3bd5877949b17f3b82efb26a2901714
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-05 11:07:19 -0400

    feat(core): Interface "template-schema" (spec)

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -1,4 +1,7 @@
+import { TemplateSchema } from "./template-schema";
+
 export interface ArmariosPaginaCoreOptions {
+  template: TemplateSchema;
   sources: string[];
   write?: string;
   strict: true;
