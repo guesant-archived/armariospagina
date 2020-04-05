a65fbd0b146017cb07901a218d2eed4d77a27108
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-05 11:35:07 -0400

    feat(core): Lib for get width and height by Jimp Image

--- packages/core/src/lib/index.ts
+++ packages/core/src/lib/index.ts
@@ -1,5 +1,6 @@
 import * as assert from './node-assert';
 import loadJimp from './load-jimp';
+import imageSizeFromJimp from './image-size-from-jimp';
 import jimpApplyFx from './jimp-apply-fx';
 
-export { assert, loadJimp, jimpApplyFx };
+export { assert, loadJimp, imageSizeFromJimp, jimpApplyFx };
