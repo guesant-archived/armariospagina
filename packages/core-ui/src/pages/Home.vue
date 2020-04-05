f38c8218dbc5b08712b4046736d5a033a3e7dde8
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-04 21:38:20 -0400

    feat(core-ui): Home/Footer

--- packages/core-ui/src/pages/Home.vue
+++ packages/core-ui/src/pages/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="font-app bg-gray-900 text-white">
     <Intro />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Intro from '@/pages/Home/Intro.vue';
+import Footer from '@/pages/Home/Footer.vue';
 
 export default {
   components: {
     Intro,
+    Footer
   }
 }
 </script>
