7f30c1365fc1a620a112bef82b7e8eab0375c36c
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-05 11:57:27 -0400

    feat(core): Lib to get image size by [x1, y1], [x2, y2]

--- packages/core/src/lib/index.ts
+++ packages/core/src/lib/index.ts
@@ -1,6 +1,13 @@
 import * as assert from './node-assert';
 import loadJimp from './load-jimp';
 import imageSizeFromJimp from './image-size-from-jimp';
+import imageSizeFromPostion from './image-size-from-postion';
 import jimpApplyFx from './jimp-apply-fx';
 
-export { assert, loadJimp, imageSizeFromJimp, jimpApplyFx };
+export {
+  assert,
+  loadJimp,
+  imageSizeFromJimp,
+  imageSizeFromPostion,
+  jimpApplyFx,
+};
