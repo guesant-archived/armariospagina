4660f442cbd8f0adadc2790f11cff6ee4a6af7f8
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-04 21:49:55 -0400

    feat(core-ui): Definir extenção 40 para min-w

--- packages/core-ui/tailwind.config.js
+++ packages/core-ui/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   theme: {
+    minWidth: {
+      "40": "10rem",
+    },
     fontFamily: {
       "app": ["'Open Sans'", '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', '"Helvetica Neue"', 'sans-serif']
     }
