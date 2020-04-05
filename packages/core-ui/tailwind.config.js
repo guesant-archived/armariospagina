c2e991d82f4eea0d3cb5bbc2bf76a671b4ed2feb
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-04 21:48:30 -0400

    feat(core-ui): Usar a fonte "Open Sans" como principal.

--- packages/core-ui/tailwind.config.js
+++ packages/core-ui/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      "app": ["'Open Sans'", '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', '"Helvetica Neue"', 'sans-serif']
+    }
   },
   variants: {},
   plugins: [],
