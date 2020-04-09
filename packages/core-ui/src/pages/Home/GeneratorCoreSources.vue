626eecdcbe1f3b05eca87b5a04a2060403d6c42e
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-09 14:47:08 -0400

    feat(core-ui): GeneratorCoreSources (WIP)

---
+++ packages/core-ui/src/pages/Home/GeneratorCoreSources.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="font-bold p-3 pb-0">
+      <p>SOURCES</p>
+    </div>
+    <div class="p-3">
+      <div v-if="sources.length >= 1">
+        <ul>
+          <SourceManager v-for="(_, sourceidx) in sources"
+            :key="`gallery-source-${sourceidx}`" :sourceidx="sourceidx" />
+        </ul>
+      </div>
+      <div v-else>Você pode selecionar sources abaixo ou enviar de seu dispositivo.</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import SourceManager from '@/components/Generator/SM.vue';
+
+export default {
+  components: {
+    SourceManager
+  },
+  computed: mapGetters({
+    sources: 'generator/sources'
+  })
+}
+</script>
