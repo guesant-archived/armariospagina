c471e0738340469c0c827f4ca9e510c4ef678d09
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-09 19:10:35 -0400

    feat(core-ui): Load template on click

--- packages/core-ui/src/pages/Home/GalleryTemplates.vue
+++ packages/core-ui/src/pages/Home/GalleryTemplates.vue
@@ -10,7 +10,12 @@
       <div>
         <GalleryList>
           <GalleryListItem v-for="(i, idx) in baseFiles" :key="idx" :liIndex="idx+1">
-            <img class="h-full w-full object-cover" v-lazy="i" :alt="`Source ${idx+1}`" />
+            <img
+              @click="setTemplate(i)"
+              class="h-full w-full object-cover"
+              v-lazy="i"
+              :alt="`Source ${idx+1}`"
+            />
           </GalleryListItem>
         </GalleryList>
       </div>
@@ -27,6 +32,7 @@
 </template>
 
 <script>
+import unfetch from 'unfetch';
 import { mapState, mapActions } from 'vuex';
 import Gallery from '@/components/Gallery.vue';
 import GalleryList from '@/components/GalleryList.vue';
@@ -52,7 +58,32 @@ export default {
   methods: {
     ...mapActions('gallery', [
       'changePage'
-    ])
+    ]),
+    ...mapActions('generator', [
+      'loadTemplate'
+    ]),
+    async setTemplate(i) {
+
+      const { VUE_APP_GALLERY_URL } = process.env;
+
+      const idx = i.slice(i.lastIndexOf('/base-') + 6, i.lastIndexOf('.'))
+      const templateBase = `${VUE_APP_GALLERY_URL}/gallery/templates`;
+      const url = `${templateBase}/template-${idx}.json`;
+
+      this.$store.commit('generator/setMessage', 'Carregando template...');
+      const template = await unfetch(url).then(r => r.json());
+
+      template.base = template.base.replace('@gallery.templates@', `${templateBase}/`);
+      template.sources = template.sources.map(source => {
+        const sourceCopy = { ...source };
+        sourceCopy.compose.start = [+sourceCopy.compose.start[0], +sourceCopy.compose.start[1]]
+        sourceCopy.compose.end = [+sourceCopy.compose.end[0], +sourceCopy.compose.end[1]]
+
+        return sourceCopy;
+      })
+      this.loadTemplate(JSON.stringify(template));
+      this.$store.commit('generator/setMessage', '');
+    }
   }
 }
 </script>
