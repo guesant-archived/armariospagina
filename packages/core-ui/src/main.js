4b02917987f442c900ba92efb7a1ca9e3c126aed
Gabriel Rodrigues <jipacoding@gmail.com>
2020-04-09 20:48:25 -0400

    feat(core-ui): Install purgecss

--- packages/core-ui/src/main.js
+++ packages/core-ui/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import VueLazyload from 'vue-lazyload'
 import './registerServiceWorker';
+import './assets/tailwind.css';
 
 Vue.use(VueLazyload);
 Vue.config.productionTip = false
